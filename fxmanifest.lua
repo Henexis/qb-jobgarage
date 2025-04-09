eea8fcd2fcef2ddd0bceb3f7103feba48764293f
T5.Prime <106626643+Henexis@users.noreply.github.com>
2025-04-09 13:00:57 +0200

    fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,12 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'QB Job Garage - Hệ thống quản lý garage cho các nghề nghiệp'
+author 'T5.Prime'
+description 'QB Job Garage System'
 version '1.0.0'
 
 shared_scripts {
-    '@ox_lib/init.lua',
+    '@qb-core/shared/locale.lua',
     'config.lua'
 }
 
@@ -16,6 +17,11 @@ client_scripts {
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'server/main.lua'
+}
+
+dependencies {
+    'qb-core',
+    'oxmysql'
 }
 
 lua54 'yes'
